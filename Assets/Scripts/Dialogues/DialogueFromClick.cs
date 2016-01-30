818a1e4e23a0f1b0014cae5e66dc3efed0972a49
Ignat123 <ignat-nee@tut.by>
2016-01-31 01:08:27 +0300

    level3. light switcher

--- Assets/Scripts/Dialogues/DialogueFromClick.cs
+++ Assets/Scripts/Dialogues/DialogueFromClick.cs
@@ -1,23 +1,66 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 
-public class SimpleDialogue : MonoBehaviour {
-    public int currentSpeachNumber;
+public class DialogueFromClick : MonoBehaviour {
+
+    public SimpleControl playerControl;
+    public Animator playerAnimator;
+    public Rigidbody2D playerRigidbody;
+    public bool isOnArea = false;
+    public MonoBehaviour[] afterTakenScripts;
+    public Sprite[] elementSprites;
+
     public GameObject dialogueCanvas;
     public string[] dialogues;
     public string[] animationNames;
     public Animator speakerAnimator;
-    public bool hideOnStart;
+    public MonoBehaviour[] nextScripts;
+
+    private SpriteRenderer thisRenderer;
     private bool isNeedInitiate;
     private Text dialogueText;
     private Button nextStepButton;
-    public MonoBehaviour[] nextScripts;
+    private int currentSpeachNumber;
 
     void Start()
     {
-        if (!hideOnStart)
-            Initiate();
-        
+        thisRenderer = this.gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    void OnMouseOver()
+    {
+        thisRenderer.sprite = elementSprites[1];
+        if (isOnArea)
+        {
+            isOnArea = true;
+            if (Input.GetMouseButtonDown(0))
+            {
+                this.isNeedInitiate = true;
+                this.Update();
+            }
+        }
+    }
+
+    void OnMouseExit()
+    {
+        thisRenderer.sprite = elementSprites[0];
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.name == "CharacterCentralPoint")
+        {
+            isOnArea = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.gameObject.name == "CharacterCentralPoint")
+        {
+            isOnArea = false;
+        }
     }
 
     void Initiate()
@@ -36,12 +79,12 @@ public class SimpleDialogue : MonoBehaviour {
         currentSpeachNumber++;
         if (currentSpeachNumber >= dialogues.Length)
         {
-            isNeedInitiate = true;
+            //isNeedInitiate = true;
             dialogueCanvas.SetActive(false);
             nextStepButton.onClick.RemoveAllListeners();
             foreach (MonoBehaviour script in nextScripts)
                 script.enabled = true;
-            this.enabled = false;
+            //this.enabled = false;
             return;
         }
         dialogueText.text = dialogues[currentSpeachNumber].Replace("|n", "\n");
