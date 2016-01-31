22a7acb0d91a1b9037c56f72c093b1a1832b92a6
Ignat123 <ignat-nee@tut.by>
2016-01-31 11:11:46 +0300

    Добавил отображение дневника

---
+++ Assets/Scripts/Diary/DiaryActivator.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class DiaryActivator : MonoBehaviour {
+
+	private Canvas diaryCanvas;
+	private bool isEnabled = false;
+	private SimpleControl control;
+
+	void Start () {
+		diaryCanvas = GameObject.Find ("DiaryCanvas").GetComponent<Canvas> ();
+		control = GameObject.Find ("CharacterCentralPoint").GetComponent<SimpleControl> ();
+		this.enabled = false;
+	}
+
+	public void Turn () {
+		isEnabled = !isEnabled;
+		if (isEnabled) {
+			GameObject.Find ("CharacterCentralPoint").GetComponent<Rigidbody2D> ().velocity = new Vector2 ();
+			control.enabled = false;
+			diaryCanvas.enabled = true;
+		} else {
+			control.enabled = true;
+			diaryCanvas.enabled = false;
+		}
+	}
+}
