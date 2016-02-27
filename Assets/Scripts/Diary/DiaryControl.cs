9d3f72be9de76d5ee3223f697c241a1bd8cd5c1e
Yury <xralefx@gmail.com>
2016-02-27 17:00:43 +0300

    Added Diary controls and test scenes for them. DiaryControl and TextStorage allows to keep text entries in separated xml file (Assets/Text/diary.xml) and add entries to the diary, when needed.

---
+++ Assets/Scripts/Diary/DiaryControl.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Serialization;
+
+public class DiaryControl : MonoBehaviour
+{
+
+    public static DiaryControl control;
+
+    public Text textObject;
+
+    private List<string> entries;
+
+    void Awake()
+    {
+        if (control == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            control = this;
+            entries = new List<string>();
+        }
+        else if (control != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        textObject = textObject.GetComponent<Text>();
+    }
+
+    public void addEntry(string key)
+    {
+        entries.Add(TextStorage.control.getEntryByKey(key));
+        refreshUIText();
+    }
+
+    protected void refreshUIText()
+    {
+        textObject.text = "";
+        foreach (string textEntry in entries)
+        {
+            textObject.text += textEntry;
+            textObject.text += "\n\n";
+        }
+    }
+}
---
+++ Assets/Scripts/Diary/TestScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestScript : MonoBehaviour {
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(10,200,100,30), "addEntry"))
+        {
+            DiaryControl.control.addEntry("#diaryText1");
+        }
+    }
+}
