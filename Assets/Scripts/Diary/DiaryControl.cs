24a4e1985c1f7debabda53f05e0fe5f649398469
Yury <xralefx@gmail.com>
2016-03-07 19:38:06 +0300

    Changed Diary behavior, now it saves state of character and then use this state to generate diary entries. Added mock-up to scenes, so it's possible to start from any scene with chosen state of a character.

--- Assets/Scripts/Diary/DiaryControl.cs
+++ Assets/Scripts/Diary/DiaryControl.cs
@@ -4,15 +4,25 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Xml;
 using System.Xml.Serialization;
+using System;
+
 
 public class DiaryControl : MonoBehaviour
 {
-
     public static DiaryControl control;
-
     public Text textObject;
 
     private List<string> entries;
+
+    //Player Choices History
+    public string gender;
+    public string secondLevelChoice;
+
+    public const string GENDER_MALE = "male";
+    public const string GENDER_FEMALE = "female";
+    public const string SECOND_CHOICE_CRISIS = "crisis";
+    public const string SECOND_CHOICE_DRUGS = "drugs";
+    public const string SECOND_CHOICE_PRIDE = "pride";
 
     void Awake()
     {
@@ -31,12 +41,29 @@ public class DiaryControl : MonoBehaviour
     void Start()
     {
         textObject = textObject.GetComponent<Text>();
+        autoFillDiary();
     }
 
-    public void addEntry(string key)
+
+    //todo: think up a better way to chain diary entries
+    public void autoFillDiary()
+    {
+        entries.Clear();
+        try
+        {
+            setFirstLevelChoices();
+            setSecondLevelChoices();
+        }
+        catch (Exception e)
+        {
+            Debug.LogError(e.Message);
+        }
+        refreshUIText();
+    }
+
+    protected void addEntry(string key)
     {
         entries.Add(TextStorage.control.getEntryByKey(key));
-        refreshUIText();
     }
 
     protected void refreshUIText()
@@ -46,6 +73,62 @@ public class DiaryControl : MonoBehaviour
         {
             textObject.text += textEntry;
             textObject.text += "\n\n";
+        }
+    }
+
+    protected void setFirstLevelChoices()
+    {
+        if (!System.String.IsNullOrEmpty(this.gender))
+        {
+            if (this.gender != GENDER_MALE && this.gender != GENDER_FEMALE)
+            {
+                throw new Exception("Gender variable in DiaryControl set inproperly.");
+            }
+
+            if (this.gender == GENDER_MALE)
+            {
+                addEntry("#maleGenderMemory");
+            }
+            else if (this.gender == GENDER_FEMALE)
+            {
+                addEntry("#femaleGenderMemory");
+            }
+        }
+    }
+
+    //todo: think up a better way to chain complex diary entries, this is looks too badly
+    protected void setSecondLevelChoices()
+    {
+        if (!System.String.IsNullOrEmpty(this.gender) && !System.String.IsNullOrEmpty(this.secondLevelChoice))
+        {
+            if (this.gender == GENDER_MALE && this.secondLevelChoice == SECOND_CHOICE_CRISIS)
+            {
+                addEntry("#maleCrisis");
+            }
+            else if (this.gender == GENDER_MALE && this.secondLevelChoice == SECOND_CHOICE_DRUGS)
+            {
+                addEntry("#maleDrugs");
+            }
+            else if (this.gender == GENDER_MALE && this.secondLevelChoice == SECOND_CHOICE_DRUGS)
+            {
+                addEntry("#malePride");
+            }
+            else if (this.gender == GENDER_FEMALE && this.secondLevelChoice == SECOND_CHOICE_CRISIS)
+            {
+                addEntry("#femaleCrisis");
+            }
+            else if (this.gender == GENDER_FEMALE && this.secondLevelChoice == SECOND_CHOICE_DRUGS)
+            {
+                addEntry("#femaleDrugs");
+            }
+            else if (this.gender == GENDER_FEMALE && this.secondLevelChoice == SECOND_CHOICE_DRUGS)
+            {
+                addEntry("#femalePride");
+            }
+            else
+            {
+                throw new Exception("Gender or secondLevelChoice variable in DiaryControl set inproperly.");
+            }
         }
     }
 }
